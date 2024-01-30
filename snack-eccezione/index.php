54e5d33474b6342c34e10be4827d3fe69b6008d0
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-30 09:47:41 +0100

    creato classe con proprietà e funzione ed exception

---
+++ snack-eccezione/index.php
@@ -0,0 +1,44 @@
+<?php
+
+class Persona
+{
+    public $age;
+
+    public function __construct($_age)
+    {
+        $this->age = $this->setAge($_age);
+    }
+
+    public function setAge($_age)
+    {
+        if (!is_int($_age)) {
+            throw new Exception('non è un numero intero');
+        } else {
+            return $_age;
+        }
+    }
+}
+
+$mario = new Persona(16.6)
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <div>
+        <p><?php echo $mario->age ?></p>
+    </div>
+
+</body>
+
+</html>
